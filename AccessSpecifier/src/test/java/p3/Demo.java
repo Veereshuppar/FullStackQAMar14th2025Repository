127b7630ba9d466253407889f6f667054344e952
unknown <veereshsuppar5@gmail.com>
2025-05-02 12:26:21 +0530

    Sample Demo

---
+++ AccessSpecifier/src/test/java/p3/Demo.java
@@ -0,0 +1,11 @@
+package p3;
+
+public class Demo {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
